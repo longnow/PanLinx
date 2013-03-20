df1f270b1f58683d78d9ad9b8b3fd50edb551af7
David Kamholz <lautgesetz@gmail.com>
2013-03-19 18:52:05 -0700

    pull port from config

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', config.port || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   
