4a01046a3258f56ecaf8894ace71be9490a6bb44
David Kamholz <lautgesetz@gmail.com>
2013-04-07 23:45:26 -0700

    tweak sort order

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function subgpRoute(req, res, next) {
   var tuple = td[id];
   
   panlex.queryAll('/ex', 
-    { include: "lv", sort: ["tt", "lv.lc", "lv.vc"], range: ["td", tuple.beg, tuple.end] },
+    { include: "lv", sort: ["td", "tt", "lv.lc", "lv.vc"], range: ["td", tuple.beg, tuple.end] },
   function (err, data) {
     if (err) return next(err);
     res.render('subgp', { exxr: data.result });  
